f0a73dcb567e581cee8f117ab305ac43109cc1b3
Anri <anri0806@hotmail.co.jp>
2022-04-16 10:46:00 -0400

    modified seeds data

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -3,14 +3,14 @@ class MilestonesController < ApplicationController
   ####### Create, Update, Delete #######
 
   post "/milestones" do
-    milestone = Milestone.create(development: params[:development], notes: params[:notes], baby_id: params[:baby_id])
+    milestone = Milestone.create(development: params[:development], date: params[:date], notes: params[:notes], baby_id: params[:baby_id])
 
     milestone.to_json
   end
 
   patch "/milestones/:id" do
     milestone = Milestone.find(params[:id])
-    milestone.update(development: params[:development], notes: params[:notes])
+    milestone.update(development: params[:development], date: params[:date], notes: params[:notes])
 
     milestone.to_json
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 puts "🌱 Seeding..."
 # Seed your database here
 
-b1 = Baby.create(name: "Emma", sex: "Girl", baby_image_url: "https://media.istockphoto.com/photos/baby-girl-in-studio-shot-picture-id675896464?k=20&m=675896464&s=612x612&w=0&h=R3MOppjfZNAEHXVDrUJEjkF2vZZqj0Rg4iv5wbq26X0=", birthday: "2022-2-14")
+b1 = Baby.create(name: "Emma", sex: "Girl", baby_image_url: "https://media.istockphoto.com/photos/baby-girl-in-studio-shot-picture-id675896464?k=20&m=675896464&s=612x612&w=0&h=R3MOppjfZNAEHXVDrUJEjkF2vZZqj0Rg4iv5wbq26X0=", birthday: "2020-2-14")
 
-Milestone.create(development: "Fisrt smile!", baby_id: b1.id )
+Milestone.create(development: "Fisrt smile!", notes: "She smiled at mommy and daddy for the first time!", date: "2020-4-1",  baby_id: b1.id )
+Milestone.create(development: "Took first steps.", notes: "She took 3 steps on her own!", date: "2021-3-1",  baby_id: b1.id )
 Appointment.create(date: "2022-3-14", time: "12:00", doctor_name: "Dr. Acosta", notes: "1 month checkup", baby_id: b1.id )
 Immunization.create(vaccine: "MMR", date: "2022-3-14", baby_id: b1.id )
 

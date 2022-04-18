ac9d2a1dccf998170921a77adbadbf48a8dd2195
Anri <anri0806@hotmail.co.jp>
2022-04-17 21:52:15 -0400

    added get and modified route in milestones controller

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -2,11 +2,23 @@ class MilestonesController < ApplicationController
 
   ####### Create, Update, Delete #######
 
-  post "/milestones" do
-    milestone = Milestone.create(development: params[:development], date: params[:date], notes: params[:notes], baby_id: params[:baby_id])
+  get "/milestones" do
+    milestones = Milestone.all
+    milestones.to_json
+  end
+
+  post "/babies/:baby_id/milestones" do
+    baby = Baby.find_by(id: params[:baby_id])
+    milestone = baby.milestones.create(development: params[:development], date: params[:date], notes: params[:notes])
 
     milestone.to_json
   end
+
+  # post "/milestones" do
+  #   milestone = Milestone.create(development: params[:development], date: params[:date], notes: params[:notes], baby_id: params[:baby_id])
+
+  #   milestone.to_json
+  # end
 
   patch "/milestones/:id" do
     milestone = Milestone.find(params[:id])

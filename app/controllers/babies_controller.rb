4789f9642fd80f3e9f1d4e47518490cf8d5d1190
Anri <anri0806@hotmail.co.jp>
2022-12-07 15:16:21 -0500

    merged frontend repo with backend

--- app/controllers/babies_controller.rb
+++ app/controllers/babies_controller.rb
@@ -34,39 +34,3 @@ class BabiesController < ApplicationController
 
 end
 
-
-class BabiesController < ApplicationController
-
-  #####  Read, Create, Patch #####
-  
-   get "/babies" do
-     babies = Baby.all
-     babies.to_json(include: [:milestones, :appointments, :immunizations])
-   end
- 
-   get "/babies/:id" do
-     baby = Baby.find(params[:id])
-     baby.to_json(include: [:milestones, :appointments, :immunizations])
- 
-   end
- 
-   post "/babies" do
-     baby = Baby.create(name: params[:name], sex: params[:sex], baby_image_url: params[:baby_image_url], birthday: params[:birthday])
- 
-     baby.to_json
-   end
- 
-   patch "/babies/:id" do
-     baby = Baby.find(params[:id])
-     baby.update(name: params[:name], sex: params[:sex], baby_image_url: params[:baby_image_url], birthday: params[:birthday])
- 
-     baby.to_json
-   end
- 
-   delete "/babies/:id" do
-     baby = Baby.find(params[:id])
-     baby.destroy
-     baby.to_json
-   end
- 
- end
